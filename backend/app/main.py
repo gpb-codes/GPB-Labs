609777757c245d6a98a92841f22182fd9627d1ec
gpb-codes <g.pedreros.becerra@gmail.com>
2026-01-03 20:56:20 -0300

    desde 0
    dfdf

--- backend/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.routes import health
-from app.database import Base, engine
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="GPB Mini Aula Virtual API")
-
-app.include_router(health.router, prefix="/api")
